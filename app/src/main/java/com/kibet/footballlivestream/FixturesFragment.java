eb54dc0bae7731ac9ee4c2af9900ccc37febf676
K-kibet <charleykibet254@gmail.com>
2023-06-12 00:19:42 +0300

    Initial commit

--- app/src/main/java/com/kibet/footballlivestream/FixturesFragment.java
+++ app/src/main/java/com/kibet/footballlivestream/FixturesFragment.java
@@ -8,12 +8,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.JsonObjectRequest;
@@ -39,7 +41,6 @@ public class FixturesFragment extends Fragment {
     private String url;
     AlertDialog.Builder builder;
     AlertDialog alertDialog;
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -49,6 +50,7 @@ public class FixturesFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
         url = "https://api.football-data.org/v4/matches";
         recyclerView = view.findViewById(R.id.recyclerView);
         matchList = new ArrayList<>();
